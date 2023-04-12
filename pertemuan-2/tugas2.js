06fe9c02d59480dc933d72347df15dab0ced2435
HardiantiRamli <119602779+HardiantiRamli@users.noreply.github.com>
2023-04-12 12:34:11 +0700

    Tugas_21104410021_Hardianti Ramli

---
+++ pertemuan-2/tugas2.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(254, 245, 231);
+  fill(52, 73, 94);
+  rect(200, 200, 200, 150);
+  fill(192, 57, 43);
+  triangle(200, 200, 300, 100, 400, 200);
+  fill(133, 146, 158);
+  rect(300, 250, 80, 100,);
+  fill(255);
+  ellipse(370, 300, 10, 10,)
+}
